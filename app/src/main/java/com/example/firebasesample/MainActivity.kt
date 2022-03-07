66e3d9dbcd007b6a968971ae6154f1d5f02e2724
Mina <you@example.com>
2022-03-07 15:39:37 +0100

    setUp LogOut Firebase

--- app/src/main/java/com/example/firebasesample/MainActivity.kt
+++ app/src/main/java/com/example/firebasesample/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.firebasesample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,10 +15,13 @@ class MainActivity : AppCompatActivity() {
         val userId=intent.getStringExtra("user_id")
         val email=intent.getStringExtra("email_id")
 
-        user_id.text=userId
-        email_id.text=email
+        user_id.text="UserId::$userId"
+        email_id.text="YourEmail::$email"
         logout_button.setOnClickListener{
-
+          FirebaseAuth.getInstance().signOut()
+            val intent=Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
 
 
